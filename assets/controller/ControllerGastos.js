9eaaf85eb82ffe1f32f0c3495b3e26639708ffc1
Juanmrno <jmrno14@hotmail.com>
2023-11-24 22:08:36 -0500

    se desarrolla la accion de listar

--- assets/controller/ControllerGastos.js
+++ assets/controller/ControllerGastos.js
@@ -65,29 +65,55 @@ registrarGastos.addEventListener('click', function () {
 
 let btnGuardar = document.getElementById("btnGuardar");
 btnGuardar.addEventListener("click", () => {
-    let id = Math.random() * 100;
-    let categoria = document.getElementById("categoria").value;
-    let descripcion = document.getElementById("descripcion").value;
-    let valor = document.getElementById("valor").value;
+   let gcategoria = (document.getElementById("gcategoria").value = "");
+   let gdescripcion = (document.getElementById("gdescripcion").value = "");
+   let gvalor = (document.getElementById("gvalor").value = "");
+   let gfecha = (document.getElementById("gfecha").value = "");
 
-    let gasto = {
-        id, categoria, descripcion, valor
-    };
+   let gasto = {
+      id: Math.random() * 100,
+      categoria: gcategoria,
+      descripcion: gdescripcion,
+      valor: gvalor,
+      fecha: gfecha,
+   };
 
-    gastos.push(gasto);
-    movimientos();
+   gastos.push(gasto);
+
+   document.getElementById("gcategoria").value = "";
+   document.getElementById("gdescripcion").value = "";
+   document.getElementById("gvalor").value = "";
+   document.getElementById("gfecha").value = "";
+
+   movimientos();
 });
 
-let listarGastos = document.getElementById('listarGastos');
+let listarGastos = document.getElementById("listarGastos");
 
-listarGastos.addEventListener('click', function () {
-    document.getElementById('sectionRegistrarGastos').style.zIndex = '2';
-    document.getElementById('sectionListarGastos').style.zIndex = '3';
-    document.getElementById('sectionMovimientosGastos').style.zIndex = '2';
-    
-    gastos.map((gasto) => {
-        console.log(gasto);
-    });
+listarGastos.addEventListener("click", function () {
+   document.getElementById("sectionRegistrarGastos").style.zIndex = "2";
+   document.getElementById("sectionListar").style.zIndex = "3";
+   document.getElementById("sectionMovimientosGastos").style.zIndex = "2";
+
+   gastos.map((gasto) => {
+      let categoria = document.createElement("p");
+      categoria.textContent = gasto.categoria;
+
+      let descripcion = document.createElement("p");
+      descripcion.textContent = gasto.descripcion;
+
+      let valor = document.createElement("p");
+      valor.textContent = gasto.valor;
+
+      let fecha = document.createElement("p");
+      fecha.textContent = gasto.fecha;
+
+      let card = document.createElement("article");
+      card.classList.add("card");
+      card.append(categoria, descripcion, valor, fecha);
+
+      document.getElementById("sectionListar").append(card);
+   });
 });
 
 let movimientosGastos = document.getElementById('movimientosGastos');
